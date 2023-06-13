f5e47578952ed0c63511d33c68b2ac5c1789f991
Kelechi <kellyxglobal@gmail.com>
2023-06-13 15:14:17 -0700

    Project setup with different files and folders in the src folder  such as the the config, constants for variables, services for emails, static for css pages, templates for html pages, the __init__.py as the default page when importing from src folder etc

---
+++ src/app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+## Create an end point or a route by using a decorector as seen 
+
+##@app.get("/")
+##def index():
+        ##return "Hello world"
+
+##@app.get("/hello")
+##def say_hello():
+        ##return jsonify({"message": "Hello world"})
+##
+
