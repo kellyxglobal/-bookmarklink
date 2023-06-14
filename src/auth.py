ab147c252a47fa55fe64c26a73735853c37efad4
Kelechi <kellyxglobal@gmail.com>
2023-06-14 07:22:18 -0700

    When a user is registered or when a POST method is called via the URL FLASK will invoke the register function telling us that the user is created and getting th current logged in user

--- src/auth.py
+++ src/auth.py
@@ -3,6 +3,11 @@ from flask import Blueprint
 #Blueprint for users' authentication
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
 
+#Tell a user that his data got submitted succesfully
 @auth.post('/register')
 def register():
     return "User Created"
+
+#Get the current loged in user
+@auth.get("/me")
+def user():
