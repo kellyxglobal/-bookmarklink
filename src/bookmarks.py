e3620d39695d9756ca59c62480cb5cae9c759868
Kelechi <kellyxglobal@gmail.com>
2023-06-23 14:24:48 -0700

    retrieving and adding pginatio to my booklink api

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -52,11 +52,16 @@ def handle_bookmarks():
         }), HTTP_201_CREATED
     
     else:
-        bookmarks = Bookmark.query.filter_by(user_id=current_user)
+
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 5, type=int)
+
+        bookmarks = Bookmark.query.filter_by(user_id=current_user).paginate(page=page, per_page=per_page)
+
 
         data=[]
 
-        for bookmark in bookmarks:
+        for bookmark in bookmarks.items:
             data.append({
                 'id': bookmark.id,
                 'url': bookmark.url,
@@ -67,4 +72,15 @@ def handle_bookmarks():
                 'updated_at': bookmark.updated_at,
             })
 
-        return jsonify({'data': data}), HTTP_200_OK
+            meta = {
+                "page": bookmarks.page,
+                'pages': bookmarks.pages,
+                'total': bookmarks.total,
+                'prev_page': bookmarks.prev_num,
+                'next_page': bookmarks.next_num,
+                'has_next': bookmarks.has_next,
+                'has_prev': bookmarks.has_prev,
+                
+            }
+
+        return jsonify({'data': data, "meta": meta}), HTTP_200_OK
